1a0afd2ba4710e1fc04916f234c2a940842879d4
Edgar <egagouribe15@gmail.com>
2026-04-25 13:27:06 -0500

    Creación ObjectMapper

--- src/main/java/com/restaiuranteboard/backend/BackendApplication.java
+++ src/main/java/com/restaiuranteboard/backend/BackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.restaiuranteboard.backend.repository.sql")
